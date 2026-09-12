d538fe7500944ac9c08e1915cb067de12e23bf64
Dpka Singh <112195696+Ddeepika123@users.noreply.github.com>
2026-09-13 01:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+ const container = document.querySelector(".container");
+
+    for (let i = 0; i < 800; i++) {
+      const square = document.createElement("div");
+
+      square.classList.add("square");
+
+      square.addEventListener("mouseenter", function () {
+        square.style.backgroundColor = getRandomColor();
+
+        setTimeout(function () {
+          square.style.backgroundColor = "#eee";
+        }, 1000);
+      });
+
+      container.appendChild(square);
+    }
+
+    function getRandomColor() {
+      const letters = "0123456789ABCDEF";
+      let color = "#";
+
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+
+      return color;
+    }
